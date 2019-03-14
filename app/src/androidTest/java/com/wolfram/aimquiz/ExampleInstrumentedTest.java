3b6f9a5a29986c3dc48d395b7511d377b0003b4d
jakub215900 <lol215900>
2019-03-14 22:16:02 +0100

    Commit to new branch (develop)

--- app/src/androidTest/java/com/wolfram/aimquiz/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wolfram/aimquiz/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.aimquiz;
+package com.wolfram.aimquiz;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
---
+++ app/src/main/java/com/wolfram/aimquiz/MainActivity.java
@@ -0,0 +1,38 @@
+package com.wolfram.aimquiz;
+
+import android.database.SQLException;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DataBaseHelper myDbHelper = new DataBaseHelper(MainActivity.this);
+        myDbHelper = new DataBaseHelper(this);
+
+        try {
+
+            myDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            myDbHelper.openDataBase();
+
+        }catch(SQLException sqle){
+
+            throw sqle;
+
+        }
+    }
+}
